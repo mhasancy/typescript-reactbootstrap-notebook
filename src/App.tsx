d510b9da40c61961d1257d86dd8de297b9e4c82f
Mahadi Hasan <mhasancy@gmail.com>
2022-01-16 23:41:24 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./App.css";
+import CreateNotes from "./components/CreateNotes";
+import Header from "./components/Header";
+import NotesList from "./components/NotesList";
+import { Note } from "./models/note.model";
+
+function App() {
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: new Date().toString(),
+      title: "Travelling",
+      text: "Travelling with friends",
+      color: "#dfdfdf",
+      date: new Date().toString(),
+    },
+  ]);
+
+  return (
+    <>
+      <Header />
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <NotesList notes={notes} setNotes={setNotes} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <CreateNotes notes={notes} setNotes={setNotes} />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default App;
